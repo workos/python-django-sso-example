d8fbe01d3e4158262292c10059df509599192659
Patrick_Liao <patrick@workos.com>
2021-08-26 09:29:55 -0500

    Updated get_profile to get_profile_and_token, defined params in get_authorizationURL for clarity and added ConnectionID constant

--- sso/views.py
+++ sso/views.py
@@ -9,13 +9,19 @@ from django.urls import reverse
 
 
 workos.api_key = os.getenv('WORKOS_API_KEY')
-workos.project_id = os.getenv('WORKOS_PROJECT_ID')
+workos.client_id = os.getenv('WORKOS_CLIENT_ID')
 
 # In workos_django/settings.py, you can use DEBUG=True for local development,
 # but you must use DEBUG=False in order to test the full authentication flow
 # with the WorkOS API.
 workos.base_api_url = 'http://localhost:8000/' if settings.DEBUG else workos.base_api_url
 
+# Constants
+# Required: Fill in either domain or customer_ID or both, at least one must be populated to generate auth connection.
+# For testing purposes we fitted domain with gmail.com as an example, please edit and add domains as needed for your testing.
+CUSTOMER_EMAIL_DOMAIN = 'gmail.com'
+CONNECTION_ID = ''
+REDIRECT_URI = os.getenv('REDIRECT_URI')
 
 def login(request):
     return render(request, 'sso/login.html')
@@ -23,19 +29,18 @@ def login(request):
 
 def auth(request):
 
-    CUSTOMER_EMAIL_DOMAIN = request.GET['domain']
-    REDIRECT_URI = os.getenv('REDIRECT_URI')
     authorization_url = workos.client.sso.get_authorization_url(
-        CUSTOMER_EMAIL_DOMAIN,
-        REDIRECT_URI,
-        state={}
+        domain = CUSTOMER_EMAIL_DOMAIN,
+        redirect_uri= REDIRECT_URI,
+        state={},
+        connection= CONNECTION_ID
     )
     return redirect(authorization_url)
 
 
 def auth_callback(request):
     code = request.GET['code']
-    profile = workos.client.sso.get_profile(code)
+    profile = workos.client.sso.get_profile_and_token(code)
     profile = profile.to_dict()
 
     return JsonResponse(data=profile)
